0b8ed3accb03815e4ed9d25df7c3067c01c0d821
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2020-08-11 22:34:14 -0400

    module for keeping track of constants such as file directories

---
+++ constants.py
@@ -0,0 +1,6 @@
+DATA_DIR = 'data'
+ARCHITECTURE_DIR = '%s/architecture-data' % DATA_DIR
+RAW_DATA_DIR = '%s/raw-data' % ARCHITECTURE_DIR
+RECONSTRUCTIONS_DIR = '%s/arbor-reconstructions' % ARCHITECTURE_DIR
+FIGS_DIR = 'figs'
+DRAWINGS_DIR = '%s/drawings' % FIGS_DIR
