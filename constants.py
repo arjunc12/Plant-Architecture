7ad61726b0e0f5ea4396490605a45860d2fe4275
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2020-09-14 14:41:49 -0400

    added constants for default pareto front, and directories for storing pareto front data

--- constants.py
+++ constants.py
@@ -1,6 +1,17 @@
+import numpy as np
+
 DATA_DIR = 'data'
 ARCHITECTURE_DIR = '%s/architecture-data' % DATA_DIR
 RAW_DATA_DIR = '%s/raw-data' % ARCHITECTURE_DIR
 RECONSTRUCTIONS_DIR = '%s/arbor-reconstructions' % ARCHITECTURE_DIR
+
+RESULTS_DIR = '%s/results' % DATA_DIR
+PARETO_FRONTS_DIR = '%s/pareto-fronts' % RESULTS_DIR
+STATISTICS_DIR = '%s/statistics' % RESULTS_DIR
+
 FIGS_DIR = 'figs'
 DRAWINGS_DIR = '%s/drawings' % FIGS_DIR
+
+PARETO_FRONT_DELTA = 0.01
+DEFAULT_ALPHAS = np.arange(0, 1 + PARETO_FRONT_DELTA, PARETO_FRONT_DELTA)
+
