413a67b820bf05e59a9a8737a4bdd0cd200f8fdc
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2020-09-14 14:42:11 -0400

    added code for approximating the optimal midpoint on a line using brute force

--- optimal_midpoint.py
+++ optimal_midpoint.py
@@ -3,7 +3,7 @@ import numpy as np
 
 DIMENSION_COORD = {'x' : 0, 'y' : 1, 'z' : 2}
 
-def angleBetweenVectors(v1, v2):
+def angle_between_vectors(v1, v2):
     numerator = np.dot(v1, v2)
 
     m1 = np.sqrt(np.dot(v1, v1))
@@ -12,10 +12,10 @@ def angleBetweenVectors(v1, v2):
 
     return np.arccos(numerator / denominator)
 
-def rotationAngle(v, dim='y'):
+def rotation_angle(v, dim='y'):
     pass
 
-def translateToOrigin(p0, p1, q):
+def translate_to_origin(p0, p1, q):
     assert len(p0) == len(p1) == len(q)
 
     p0_shift = p0[:]
@@ -23,12 +23,12 @@ def translateToOrigin(p0, p1, q):
     q_shift = q[:]
 
     for i, coord in enumerate(p0):
-        for point in [p0Shift, p1Shift, qShift]:
+        for point in [p0_shift, p1_shift, q_shift]:
             point[i] -= coord
 
-    return p0Shift, p1Shirt, qShift
+    return p0_shift, p1_shirt, q_shift
 
-def optimalMidpointApprox(p0, p1, q, alpha):
+def optimal_midpoint_approx(p0, p1, q, alpha):
     assert len(p0) == len(p1) == len(q)
 
     p0 = np.array(p0)
@@ -38,15 +38,16 @@ def optimalMidpointApprox(p0, p1, q, alpha):
     m = p1 - p0
 
     delta = 0.01
-    bestCost = None
-    bestMidpoint = None
+    best_cost = None
+    best_midpoint = None
     for t in np.arange(0, 1 + delta, delta):
         midpoint = p0 + m * t
         wiring =  pylab.euclidean(midpoint, q)
         delay = pylab.euclidean(midpoint, q) + pylab.euclidean(p0, midpoint)
         cost = (alpha * wiring) + ((1 - alpha) * delay)
 
-        if bestCost == None or cost < bestCost:
-            bestCost = cost
-            bestMidpoint = midpoint
+        if best_cost == None or cost < best_cost:
+            best_cost = cost
+            best_midpoint = midpoint
 
+    return best_cost, best_midpoint
