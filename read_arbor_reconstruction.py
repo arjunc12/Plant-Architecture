d0eea465cba0034c4aeafc47ea4dd0c6abf396ea
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2020-09-14 14:43:37 -0400

    modified code to deal with full arbor tracings

--- read_arbor_reconstruction.py
+++ read_arbor_reconstruction.py
@@ -1,14 +1,81 @@
 import networkx as nx
 from scipy.spatial.distance import euclidean
-from utils import connect_insertions
+from utils import connect_insertions, toy_network, draw_arbor
 from constants import RECONSTRUCTIONS_DIR, DRAWINGS_DIR
 import os
-import pylab
 import pandas as pd
 
-NODE_SIZE = {'main root' : 50, 'lateral root' : 30, 'insertion point' : 10}
+def connect_lateral_roots(G, root_points, lateral_starts):
+    root_points = sorted(root_points, key = lambda p: p[1])
+    for lateral_start in lateral_starts:
+        assert G.has_node(lateral_start)
+        closest_dist = float("inf")
+        closest_point = None
+        y_lateral = lateral_start[1]
 
-def read_arbor(fname):
+        for root_point in root_points:
+            y_root = root_point[1]
+            if y_root > y_lateral:
+                assert closest_point != None
+                break
+            else:
+                dist = euclidean(lateral_start, root_point)
+                if dist < closest_dist:
+                    closest_dist = dist
+                    closest_point = root_point
+
+        assert closest_point != None
+        assert G.has_node(closest_point)
+        assert G.node[closest_point]['label'] == 'main root'
+        assert not G.has_edge(closest_point, lateral_start)
+
+        G.add_edge(closest_point, lateral_start)
+        G[closest_point][lateral_start]['length'] = closest_dist
+
+def read_arbor_full(fname):
+    G = nx.Graph()
+    G.graph['arbor name'] = fname.strip('.csv')
+    prev_point = None
+    curr_root = None
+    root_points = []
+    lateral_starts = []
+
+    with open('%s/%s' % (RECONSTRUCTIONS_DIR, fname)) as f:
+        for line in f:
+            line = line.strip('\n')
+            line = line.split(',')
+            if len(line) == 1:
+                curr_root = line[0]
+                prev_point = None
+            else:
+                point = tuple(map(float, line))
+                if prev_point == None:
+                    G.add_node(point)
+                    lateral_starts.append(point)
+                    if curr_root == 'lateral root':
+                        lateral_starts.append(point)
+                else:
+                    G.add_edge(prev_point, point)
+                    G[prev_point][point]['length'] = euclidean(prev_point, point)
+                    print(curr_root, prev_point, point)
+
+                if curr_root == 'main root':
+                    G.node[point]['label'] = 'main root'
+                    root_points.append(point)
+                else:
+                    G.node[point]['label'] = 'lateral root'
+
+                prev_point = point
+
+    connect_lateral_roots(G, root_points, lateral_starts)
+
+    for u in G.nodes():
+        if G.node[u]['label'] == 'lateral root' and G.degree(u) == 1:
+            G.node[u]['label'] = 'lateral root tip'
+
+    return G
+
+def read_arbor_condensed(fname):
     df = pd.read_csv('%s/%s' % (RECONSTRUCTIONS_DIR, fname), skipinitialspace=True)
     root_order = df['root order']
     x_coord = df['x coordinate']
@@ -21,59 +88,26 @@ def read_arbor(fname):
     for order, x, y, insertion in zip(root_order, x_coord, y_coord, insertion_point):
         p1 = (x, y)
         p2 = (0, insertion)
-        G.add_edge(p1, p2)
         if order == 0:
+            G.add_node(p1)
             G.node[p1]['label'] = 'main root'
-        elif order == 1:
-            G.node[p1]['label'] = 'lateral root'
+            G.graph['main root'] = p1
 
-        G.node[p2]['label'] = 'insertion point'
-        G[p1][p2]['length'] = euclidean(p1, p2)
+        elif order == 1:
+            G.add_edge(p1, p2)
+            G.node[p1]['label'] = 'lateral root'
+            G.node[p2]['label'] = 'insertion point'
+
+            G[p1][p2]['length'] = euclidean(p1, p2)
 
 
     connect_insertions(G)
 
     return G
 
-def draw_arbor(G, outdir):
-    pos = {}
-    nodelist = []
-    node_size = []
-
-    xmin = float("inf")
-    xmax = float("-inf")
-    ymin = float("inf")
-    ymax = float("-inf")
-
-    for u in G.nodes():
-        x, y = u
-        pos[u] = (x, y)
-
-        xmin = min(xmin, x)
-        xmax = max(xmax, x)
-        ymin = min(ymin, y)
-        ymax = max(ymax, y)
-
-        nodelist.append(u)
-        node_size.append(NODE_SIZE[G.node[u]['label']])
-
-    pylab.figure()
-    nx.draw_networkx_nodes(G, pos=pos, nodelist=nodelist, node_color='k', node_size=node_size)
-    nx.draw_networkx_edges(G, pos=pos, width=5, edge_color='g')
-    pylab.draw()
-
-    #pylab.xlim(xmin - 0.1, 1, xmax + 0.1)
-    #pylab.ylim(xmin - 0.1, xmax + 0.1)
-    ax = pylab.gca()
-    ax.axis("off")
-
-    os.system('mkdir -p %s' % outdir)
-    pylab.savefig('%s/%s.pdf' % (outdir, G.graph['arbor name']), format='pdf')
-    pylab.close()
-
 def main():
-    fname = '060_1_C_day2.csv'
-    G = read_arbor(fname)
+    G =read_arbor_full('272_3_S_day3.csv')
+    #G =read_arbor_full('165_3_S_day3.csv')
     draw_arbor(G, DRAWINGS_DIR)
 
 if __name__ == '__main__':
