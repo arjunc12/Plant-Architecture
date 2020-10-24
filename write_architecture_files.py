ad0ba0f231dd81d31c553b3ca40cd58d5d625236
arjun chandrasekhar <arjunc12@gmail.com>
2020-10-24 13:07:02 -0400

    cleaned up code

--- write_architecture_files.py
+++ write_architecture_files.py
@@ -13,15 +13,8 @@ def write_arbor_file_condensed(output_fname, points):
         for root_type, x, y, insertion in points:
             f.write('%d, %f, %f, %f\n' % (root_type, x, y, insertion))
 
-def write_arbor_file_full(output_fname, root_points, lateral_roots, day, root_name,
-                          image_number, root_condition):
+def write_arbor_file_full(output_fname, root_points, lateral_roots):
     with open(output_fname, 'w') as f:
-        # write the metadata
-        f.write('name: %s\n' % name)
-        f.write('day: %s\n' %  day)
-        f.write('image number: %s\n' % image_number)
-        f.write('root condition: %s\n' % root_condition)
-
         # write the main root points
         f.write('main root\n')
         for x, y in root_points:
@@ -33,52 +26,56 @@ def write_arbor_file_full(output_fname, root_points, lateral_roots, day, root_na
             for x, y in points:
                 f.write('%f, %f\n' % (x, y))
 
-def get_day(image_name):
-    day_pos = image_name.index('day')
-    return image_name[day_pos:day_pos + 4]
-
-def get_image_num(image_name):
-    image_name = image_name.strip('_')
-    image_name = image_name.split('_')
-    return int(image_name[3])
-
-def get_condition(root_name):
+def root_name_metadata(root_name):
     root_name = root_name.split('_')
-    return root_name[2]
+    genotype = root_name[0]
+    replicate = root_name[1]
+    condition = root_name[2]
+    return genotype, replicate, condition
+
+def image_metadata(image):
+    image = image.strip('_')
+    image = image.strip('.rsml')
+
+    image = image.split('_')
+    day = image[1]
+    picture_num = image[3]
+
+def get_day(image):
+    image = image.strip('_')
+    image_items = image.split('_')
+    return image_items[1]
 
 def write_arbor_files_full(raw_data_fname, reconstruction_dir):
     df = pd.read_csv(raw_data_fname, skipinitialspace=True)
-    root_id = df['root']
-    root_name = df['root_name']
+    images = df['image']
+    root_ids = df['root']
+    root_names = df['root_name']
     x_pos = df['x']
     y_pos = df['y']
-    root_order = df['root_order']
-    root_ontology = df['root_ontology']
-    image_day = df['image'].apply(get_day)
-    image_number = df['image'].apply(get_image_num)
-    root_condition = df['root_name'].apply(get_condition)
+    root_orders = df['root_order']
 
     curr_name = None
-    curr_fname = None
+    curr_day = None
     curr_root_points = None
     curr_lateral_roots = None
 
-    for id, name, x, y, day, number, condition, order, ontology in zip(root_id, root_name,
-        x_pos, y_pos, image_day, image_number, root_condition, root_order, root_ontology):
-        if order == 0:
-            assert ontology == 'Root'
+    for image, root_id, root_name, x, y, root_order in\
+        zip(images, root_ids, root_names, x_pos, y_pos, root_orders):
+
+        if root_order == 0:
             # check if we've found a the main root for a new arbor
-            if curr_name != name:
+            if root_name != curr_name:
                 # check if this is not the first arbor
                 if curr_lateral_roots != None and len(curr_lateral_roots) > 1:
                     # if not the first main root, write the architecture file for the previous arbor
-                    write_arbor_file_full(curr_fname, curr_root_points, curr_lateral_roots,\
-                                          name, day, number, condition)
+                    reconstruction_fname = '%s_%s' % (curr_name, curr_day)
+                    output_fname = '%s/%s.csv' % (reconstruction_dir, reconstruction_fname)
+                    write_arbor_file_full(output_fname, curr_root_points, curr_lateral_roots)
 
                 # reset the points associated with the current (new) arbor
-                curr_name = name
-                reconstruction_fname = '%s_%s' % (name, day)
-                curr_fname = '%s/%s.csv' % (reconstruction_dir, reconstruction_fname)
+                curr_name = root_name
+                curr_day = get_day(image)
                 curr_root_points = [(x, y)]
                 curr_lateral_roots = defaultdict(list)
             else:
@@ -86,8 +83,7 @@ def write_arbor_files_full(raw_data_fname, reconstruction_dir):
                 curr_root_points.append((x, y))
         else:
             # the next point is associated with a lateral root for our current arbor
-            assert ontology == 'Lateral root'
-            curr_lateral_roots[id].append((x, y))
+            curr_lateral_roots[root_id].append((x, y))
 
 def write_arbor_files_condensed(raw_data_fname, reconstruction_dir):
     df = pd.read_csv(raw_data_fname, skipinitialspace=True)
