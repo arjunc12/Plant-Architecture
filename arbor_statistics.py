68c9366a20d55d7c0abfabb1e9bfecda63c22819
arjun chandrasekhar <arjunc12@gmail.com>
2020-10-27 15:22:17 -0400

    start working on line plot code

--- arbor_statistics.py
+++ arbor_statistics.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import pylab
-from constants import *
+from constants import STATISTICS_DIR, PLOTS_DIR, METADATA_DIR
 import seaborn as sns
+sns.set()
 
 sns.set()
 
 def get_dfs():
-    pareto_front_stats = pd.read_csv('%s/arbor_stats.csv' % STATISTICS_DIR, skipinitialspace=True)
-
-    return pareto_front_stats
+    pareto_front_df = pd.read_csv('%s/arbor_stats.csv' % STATISTICS_DIR, skipinitialspace=True)
+    metadata_df = pd.read_csv('%s/metadata.csv' % METADATA_DIR, skipinitialspace=True)
+    return pareto_front_df, metadata_df
 
 def alphas_hist(df):
     pylab.figure()
@@ -18,9 +19,21 @@ def alphas_hist(df):
     pylab.savefig('%s/alpha_hist.pdf' % PLOTS_DIR, format='pdf')
     pylab.close()
 
+def alpha_time_plot(pareto_front_df, metadata_df):
+    df = pd.merge(pareto_front_df, metadata_df)
+    pylab.figure()
+    ax = pylab.gca()
+    for name, group in df.groupby(['genotype', 'replicate']):
+        if len(group['day'].unique()) < 3:
+            continue
+        sns.lineplot(x='day', y='pareto front location', hue='condition', data=group, ax=ax)
+    pylab.tight_layout()
+    pylab.savefig('%s/alpha_time_plot.pdf' % PLOTS_DIR, format='pdf')
+    pylab.close()
+
 def main():
-    pareto_front_stats = get_dfs()
-    alphas_hist(pareto_front_stats)
+    pareto_front_df, metadata_df = get_dfs()
+    alpha_time_plot(pareto_front_df, metadata_df)
 
 if __name__ == '__main__':
     main()
