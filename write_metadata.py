d0a4d9e873b96716bf81abdfd752773975f170eb
arjun chandrasekhar <arjunc12@gmail.com>
2020-10-27 15:24:28 -0400

    code for creating metadata file from the tracings

--- write_metadata.py
+++ write_metadata.py
@@ -1,29 +1,10 @@
 import os
-
-def image_metadata(image):
-    image = image.strip('_')
-    image = image.strip('.rsml')
-
-    image_items = image.split('_')
-
-    day = image_items[1]
-    day = day.strip('day')
-
-    picture_num = image_items[-1]
-
-    return day, picture_num
-
-def root_name_metadata(root_name):
-    root_name_items = root_name.split("_")
-
-    genotype = root_name_items[0]
-    replicate = root_name_items[1]
-    condition = root_name_items[2]
-
-    return genotype, replicate, condition
+import utils
+import pandas as pd
+from constants import RAW_DATA_DIR, METADATA_DIR
 
 def write_metadata(tracing_fname, output_fname):
-    df = pd.read_csv(tracing_fnamem skipinitialspace=True)
+    df = pd.read_csv(tracing_fname, skipinitialspace=True)
     df = df[df['root_order'] == 0]
     df = df[['image', 'root_name', 'root_order']]
     df.drop_duplicates(inplace=True)
@@ -32,4 +13,17 @@ def write_metadata(tracing_fname, output_fname):
         f.write('arbor name, day, Picture #, genotype, replicate, condition\n')
 
         for image, root_name in zip(df['image'], df['root_name']):
-            pass
+            day, picture_num = utils.image_metadata(image)
+            genotype, replicate, condition = utils.root_name_metadata(root_name)
+
+            arbor = utils.arbor_name(image, root_name)
+
+            f.write('%s, %s, %s, %s, %s, %s\n' % (arbor, day, picture_num, genotype, replicate, condition))
+
+def main():
+    tracing_fname = '%s/%s' % (RAW_DATA_DIR, 'pimpiBig2_D0D1D2-full-tracing.csv')
+    output_fname = '%s/%s' % (METADATA_DIR, 'metadata.csv')
+    write_metadata(tracing_fname, output_fname)
+
+if __name__ == '__main__':
+    main()
