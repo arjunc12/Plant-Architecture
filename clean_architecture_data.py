17d47feb4d217924770e10fa11be621d447e7ba6
arjun chandrasekhar <arjunc12@gmail.com>
2020-11-20 10:18:11 -0500

    code to check that main roots are labelled correctly and images are in order

--- clean_architecture_data.py
+++ clean_architecture_data.py
@@ -1,23 +1,47 @@
 import pandas as pd
 from constants import CLEANED_ROOT_NODES_DIR
 import os
+import argparse
 
 def count_root_name_items(root_name):
+    root_name = str(root_name)
     return len(root_name.split('_'))
 
-def print_incorrect_rows(df):
+def print_incorrect_main_roots(df):
     df = df[['root_name', 'root_order']]
     df = df[df['root_order'] == 0]
     df['root order items'] = df['root_name'].apply(count_root_name_items)
     df = df[df['root order items'] != 3]
     print(df)
 
-def clean_dataset(fname):
-    df = pd.read_csv(fname, skipinitialspace=True)
-    print_incorrect_rows(df)
+def print_incorrect_lateral_roots(df):
+    df = df[['root_name', 'root_order']]
+    df = df[df['root_order'] == 1]
+    df['root order items'] = df['root_name'].apply(count_root_name_items)
+    df = df[df['root order items'] != 2]
+    print(df)
 
-def clean_data():
+def print_incorrect_rows(fname):
+    df = pd.read_csv(fname, skipinitialspace=True)
+    print_incorrect_main_roots(df)
+    print_incorrect_lateral_roots(df)
+
+def check_image_order(full_fname):
+    df = pd.read_csv(full_fname, skipinitialspace=True)
+    images = df['image']
+
+    prev_images = set()
+    curr_image = None
+
+    for image in images:
+        if image != curr_image:
+            assert image not in prev_images
+            prev_images.add(curr_image)
+            curr_image = image
+
+def clean_data(args):
     for fname in os.listdir(CLEANED_ROOT_NODES_DIR):
+
         if fname.endswith('csv'):
             print('+' + ('-' * (len(fname) + 2)) + '+')
             print('+ ' + fname + ' +')
@@ -25,10 +49,17 @@ def clean_data():
 
             full_fname = '%s/%s' % (CLEANED_ROOT_NODES_DIR, fname)
 
-            clean_dataset(full_fname)
+            if args.incorrect:
+                print_incorrect_rows(full_fname)
+
+            if args.image_order:
+                check_image_order(full_fname)
 
 def main():
-    clean_data()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--incorrect', action='store_true')
+    parser.add_argument('--image_order', action='store_true')
+    clean_data(parser.parse_args())
 
 if __name__ == '__main__':
     main()
