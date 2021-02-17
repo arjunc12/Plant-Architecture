2279604e325cd7ad0f44757f41ffb180a7e5e781
arjun chandrasekhar <arjunc12@gmail.com>
2021-02-16 22:11:09 -0500

    added command line options

--- arbor_statistics.py
+++ arbor_statistics.py
@@ -3,6 +3,7 @@ import pylab
 from constants import STATISTICS_DIR, PLOTS_DIR, METADATA_DIR, SCORING_DATA_DIR
 import seaborn as sns
 import pingouin as pg
+import argparse
 
 IDEOTYPES = {'T' : 'Telephone', 'dT' : 'Droopy Telephone', 'C' : 'Christmas Tree', 'B' : 'Broomstick'}
 
@@ -27,9 +28,9 @@ def alphas_hist(df, hue='condition'):
     pylab.savefig('%s/alpha_hist.pdf' % PLOTS_DIR, format='pdf')
     pylab.close()
 
-def alpha_distribution_plot(df):
+def alpha_distribution_plot(df, hue="condition"):
     pylab.figure()
-    sns.boxenplot(x='ideotype', y='pareto front location', hue='condition', data=df)
+    sns.boxenplot(x='ideotype', y='pareto front location', hue=hue, data=df)
     pylab.tight_layout()
     pylab.savefig('%s/alpha_distribution.pdf' % PLOTS_DIR, format='pdf')
     pylab.close()
@@ -54,14 +55,33 @@ def alpha_anova(df):
     print(pg.welch_anova(data=df, dv='pareto front location', between='ideotype'))
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--histogram', action='store_true')
+    parser.add_argument('--distribution', action='store_true')
+    parser.add_argument('--time', action='store_true')
+    parser.add_argument('--anova', action='store_true')
+
+    args = parser.parse_args()
+
     pareto_front_df, metadata_df, ideotypes_df = get_dfs()
     arbor_stats_df = pd.merge(pareto_front_df, metadata_df)
 
-    last_day_arbors = last_day_df(arbor_stats_df)
-    arbor_ideotypes = pd.merge(last_day_arbors, ideotypes_df)
-    alpha_distribution_plot(arbor_ideotypes)
 
-    alpha_time_plot(arbor_stats_df)
+    last_day_arbors = last_day_df(arbor_stats_df)
+
+    arbor_ideotypes = pd.merge(last_day_arbors, ideotypes_df)
+
+    if args.histogram:
+        alphas_hist(arbor_ideotypes, hue="ideotype")
+
+    if args.distribution:
+        alpha_distribution_plot(arbor_ideotypes, hue=None)
+
+    if args.time:
+        alpha_time_plot(arbor_stats_df)
+
+    if args.anova:
+        alpha_anova(arbor_ideotypes)
 
 if __name__ == '__main__':
     main()
