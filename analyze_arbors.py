7894f5db679cefbc39e62f003b99011f60e459b2
arjun chandrasekhar <arjunc12@gmail.com>
2021-02-16 22:09:31 -0500

    code for writing parato data about the arbors

--- analyze_arbors.py
+++ analyze_arbors.py
@@ -1,9 +1,10 @@
 from constants import *
 from read_arbor_reconstruction import read_arbor_full
-from pareto_functions import pareto_front, pareto_dist
+from pareto_functions import pareto_front, pareto_dist, pareto_dist_scale
 import numpy as np
 import os
 import pandas as pd
+import argparse
 
 def write_front(G, outdir, alphas, wiring_costs, conduction_delays):
     fname = G.graph['arbor name']
@@ -50,8 +51,45 @@ def analyze_arbors():
             dist, alpha = pareto_dist(G, alphas, wiring_costs, conduction_delays)
             f.write('%s, %f, %f\n' % (arbor_name, dist, alpha))
 
+def write_scaling_dists():
+    alphas = DEFAULT_ALPHAS
+    fname = '%s/scaling_distances.csv' % STATISTICS_DIR
+    first_time = not os.path.exists(fname)
+
+    prev_arbors = []
+    if not first_time:
+        df = pd.read_csv(fname, skipinitialspace=True)
+        prev_arbors = list(df['arbor name'])
+
+    with open(fname, 'a') as f:
+        if first_time:
+            f.write('arbor name, pareto front scaling distance, pareto front scaling location')
+
+        for arbor_fname in os.listdir(RECONSTRUCTIONS_DIR):
+            if arbor_fname.strip('.csv') in prev_arbors:
+                continue
+
+            print(arbor_fname)
+
+            G = read_arbor_full(arbor_fname)
+            arbor_name = G.graph['arbor name']
+
+            wiring_costs, conduction_delays = get_pareto_front(G, alphas)
+
+            dist, alpha = pareto_dist_scale(G, alphas, wiring_costs, conduction_delays)
+            f.write('%s, %f, %f\n' % (arbor_name, dist, alpha))
+
 def main():
-    analyze_arbors()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--analyze', action='store_true')
+    parser.add_argument('--scaling', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.analyze:
+        analyze_arbors()
+    if args.scaling:
+        write_scaling_dists()
 
 if __name__ == '__main__':
     main()
