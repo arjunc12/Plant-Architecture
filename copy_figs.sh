a9a236c1bfec7d32a36d13bb1eadcbbd8a29640c
arjun chandrasekhar <arjunc12@gmail.com>
2021-08-12 22:49:21 -0700

    ignore paper directories

--- copy_figs.sh
+++ copy_figs.sh
@@ -1,3 +1,3 @@
-cp figs/plots/null-models-analysis/null-models-scatterplot.pdf paper-figs/BDA2021/Results
-cp figs/plots/pareto-front-location-analysis/alpha_distribution_ideotype.pdf paper-figs/BDA2021/Results
-cp figs/plots/pareto-front-location-analysis/alpha_distribution_condition.pdf paper-figs/BDA2021/Results
+cp figs/plots/null-models-analysis/null-models-scatterplot.pdf paper-figs/Results
+cp figs/plots/pareto-front-location-analysis/alpha_distribution_ideotype.pdf paper-figs/Results
+cp figs/plots/pareto-front-location-analysis/alpha_distribution_condition.pdf paper-figs/Results
