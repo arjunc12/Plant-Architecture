05ee724e555d0d34067bf06c5fca0074b3b756d7
MissSeraphim <wigglelegend@gmail.com>
2025-05-28 12:16:34 -0500

    filled out ArborBuild class

--- Arbor.java
+++ Arbor.java
@@ -18,7 +18,7 @@ public class Arbor {
     //builds arbor from a file
     public static void main(String[] args) throws FileNotFoundException {
     	System.out.println("Running Arbor Build...");
-    	Arbor arbor = ArborBuild.buildArborFile("005_3_S_day5.csv");
+    	Arbor arbor = ArborBuild.buildArborFile("data/architecture-data/arbor-reconstructions/005_3_S_day5.csv");
     }
     
     //constructor
@@ -88,12 +88,44 @@ class Point {
 //builds an arbor from a file
 class ArborBuild {
     public static Arbor buildArborFile (String filename) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("data/architecture-data/arbor-reconstructions/005_3_S_day5.csv"));
-        //if main root, continue until lateral root is encountered
+        Scanner scanner = new Scanner(new File(filename));
         //separate coords based on comma
         //if scanner line has a dash, it is a lateral root (assuming the strings of letters/nums are IDS)
-        Arbor arbor = null;
-        return null;
+        Arbor arbor = new Arbor();
+        
+        //if main root, continue until lateral root is encountered
+        boolean readingMain = true;
+        //keeps track of sub roots
+        String currentLatID = null;
+        
+        while (scanner.hasNextLine()) {
+        	String line = scanner.nextLine().trim();
+        	
+        	//checking for sub root ID
+        	if (line.contains("-")) {
+        		currentLatID = line;
+        	}
+        	else
+        	{
+        		String[] tokens = line.split(",");
+        		//checking that p and q are present and initializing the point
+        		if (tokens.length == 2) {
+        			double p = Double.parseDouble(tokens[0]);
+        			double q = Double.parseDouble(tokens[1]);
+        			Point point = new Point(p, q);
+        			
+        			//adding lat root
+        			if (currentLatID != null) {
+        				arbor.addLatRoots(currentLatID, point);
+        			}
+        			//adding main root
+        			else {
+        				arbor.addMainRoot(point);
+        			}
+        		}
+        	}
+        }
+        return arbor;
     }
 }
 
