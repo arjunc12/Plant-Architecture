315c9a823b7a29d809c6a157e6e0e581fb9c3d50
MissSeraphim <wigglelegend@gmail.com>
2025-06-19 11:36:06 -0500

    Updated Code

--- ArborTest.java
+++ ArborTest.java
@@ -39,23 +39,38 @@ public class ArborTest {
     }
     
     private static void testBestConnections(Arbor arbor, double[] alphaValues) {
-    	for (double alpha : alphaValues) {
+    	for (double alpha = 0.0; alpha <= 1.0; alpha += 0.01) {
+    		//ensures num stability
+    		alpha = Math.round(alpha * 100.0) / 100.0;
     		System.out.println("alpha value: " + alpha);
+	
     		//stores best connection for each lat root
     		Map<String, Point> connections = BestArbor.findBestConnection(arbor, alpha);
-    		
-    		//loops through lat roots to find best connection
+    	
+			double totalWiring = 0.0;
+    		double totalDelay = 0.0;
+    	
     		for (String ID : connections.keySet()) {
-    			Point tip = arbor.getLateralRoots().get(ID).getLast();
+    			List<Point> latPoints = arbor.getLateralRoots().get(ID);
+    			Point tip = latPoints.get(latPoints.size() - 1);
     			Point conn = connections.get(ID);
-    			double wiring = tip.distanceTo(conn);
-    			double delay = BestArbor.getPathDistanceTo(arbor.getMainRoot(), conn) + conn.distanceTo(tip);
+    			
+    			double wiringCost = tip.distanceTo(conn);
+    			double conductionDelay = BestArbor.getPathDistanceTo(arbor.getMainRoot(), conn) + wiringCost;
+    			
+    			totalWiring += wiringCost;
+    			totalDelay += conductionDelay;
     			
     			System.out.println("Lateral Root: " + ID);
     			System.out.println("		Best connection: (" + String.format("%.4f", conn.p) + ", " + String.format("%.4f", conn.q) + ")");
-    			System.out.println("		Wiring cost: " + String.format("%.4f", wiring));
-    			System.out.println("		Conduction delay: " + String.format("%.4f", delay));
+    			System.out.println("		Wiring cost: " + String.format("%.4f", wiringCost));
+    			System.out.println("		Conduction delay: " + String.format("%.4f", conductionDelay));
     		}
+    			
+			//rounding to 2 decimal places
+    		String alphaStr = String.format("%.2f", alpha);
+    		String wiringStr = String.format("%.4f", totalWiring);
+    		String delayStr = String.format("%.4f", totalDelay);
     	}
     }
     
