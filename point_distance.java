01d382cbb78574c1efcef23ae53a1509d5f8dc4f
MissSeraphim <wigglelegend@gmail.com>
2025-06-24 12:08:32 -0500

    updated

--- point_distance.java
+++ point_distance.java
@@ -1,8 +1,10 @@
+import java.io.File;
+import java.io.*;
+import java.util.*;
+
 public class pointDistance {
 
-    
-
-    private static void testBestConnections(Arbor arbor, double[] alphaValues) {
+	private static void testBestConnections(Arbor arbor, double[] alphaValues) {
         for (double alpha = 0.0; alpha <= 1.0; alpha += 0.01) {
             //ensures num stability
             alpha = Math.round(alpha * 100.0) / 100.0;
@@ -15,24 +17,26 @@ public class pointDistance {
             double totalDelay = 0.0;
             
             for (String ID : connections.keySet()) {
-            List<Point> latPoints = arbor.getLateralRoots().get(ID);
-            Point tip = latPoints.get(latPoints.size() - 1);
-            Point conn = connections.get(ID);
+            	List<Point> latPoints = arbor.getLateralRoots().get(ID);
+            	Point tip = latPoints.get(latPoints.size() - 1);
+            	Point conn = connections.get(ID);
             
-            double wiringCost = tip.distanceTo(conn);
-            double conductionDelay = BestArbor.getPathDistanceTo(arbor.getMainRoot(), conn) + wiringCost;
+            	double wiringCost = tip.distanceTo(conn);
+            	double conductionDelay = BestArbor.getPathDistanceTo(arbor.getMainRoot(), conn) + wiringCost;
             
-            totalWiring += wiringCost;
-            totalDelay += conductionDelay;
+            	totalWiring += wiringCost;
+            	totalDelay += conductionDelay;
             
-            System.out.println("Lateral Root: " + ID);
-            System.out.println("		Best connection: (" + String.format("%.4f", conn.p) + ", " + String.format("%.4f", conn.q) + ")");
-            System.out.println("		Wiring cost: " + String.format("%.4f", wiringCost));
-            System.out.println("		Conduction delay: " + String.format("%.4f", conductionDelay));
+            	System.out.println("Lateral Root: " + ID);
+            	System.out.println("		Best connection: (" + String.format("%.4f", conn.p) + ", " + String.format("%.4f", conn.q) + ")");
+            	System.out.println("		Wiring cost: " + String.format("%.4f", wiringCost));
+            	System.out.println("		Conduction delay: " + String.format("%.4f", conductionDelay));
             }
             
-            //rounding to 2 decimal places
-            String alphaStr = String.format("%.2f", alpha);
-            String wiringStr = String.format("%.4f", totalWiring);
-            String delayStr = String.format("%.4f", totalDelay);
-            }
+        	//rounding to 2 decimal places
+        	String alphaStr = String.format("%.2f", alpha);
+        	String wiringStr = String.format("%.4f", totalWiring);
+        	String delayStr = String.format("%.4f", totalDelay);
+        }
+    }
+}
