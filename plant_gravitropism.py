17414e6ba95a66b8c49e0a6843ed966b2e4483cb
arjun chandrasekhar <arjunc12@gmail.com>
2026-04-14 08:21:22 -0500

    default parallel workers set to 1

--- plant_gravitropism.py
+++ plant_gravitropism.py
@@ -927,7 +927,7 @@ def main():
     parser.add_argument('--smartNumPoints', type=int, default=1)
     parser.add_argument('--smartGridSize', type=int, default=3)
 
-    parser.add_argument('--num_workers',      type=int,   default=4,
+    parser.add_argument('--num_workers',      type=int,   default=1,
                         help='Number of parallel worker processes')
 
     parser.add_argument(
