59ddd28aa549207b95d9cb0bb70897b52915b774
martineza088 <martineza@southwestern.edu>
2026-06-02 15:05:52 -0500

    Tested first draft of the pipeline with the second last day file

--- heterogeneity_analysis.py
+++ heterogeneity_analysis.py
@@ -1,2 +1,23 @@
-print("hello world!")
-print("goodbye cruel world!")
+import math
+import networkx as nx
+import plotly.graph_objs as go
+import pylab
+import plant_gravitropism as pg
+
+# VVV might need to delete later VVV
+import draw_arbors_AM as draw
+
+# --- Calculate all alpha values ( use evaluate_parameters ) ---
+# acquiring last-day-files 
+last_day_files = pg.get_last_day_files()
+fname = last_day_files[0]
+print(fname)
+
+evaluated_arbor = pg.evaluate_parameters(fname, 0, 0)
+print(f"Evaluated arbor: Wiring → {evaluated_arbor[0]}, Delay → {evaluated_arbor[1]}, Total Orthogonal → {evaluated_arbor[2]}, Total Squared Orthogonal → {evaluated_arbor[3]}")
+
+# --- Drawing out arbors (for visualization <<< will delete this step in final pipeline) ----
+
+
+# draw.plot_arbors(fname, 0, 0)
+
