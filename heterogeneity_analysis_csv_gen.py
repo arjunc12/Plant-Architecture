f8f738f297c76f1586631d28941a2ec99b153514
martineza088 <martineza@southwestern.edu>
2026-06-03 15:02:50 -0500

    New csv files generated with improved code, formats everything correctly now

--- heterogeneity_analysis_csv_gen.py
+++ heterogeneity_analysis_csv_gen.py
@@ -12,7 +12,7 @@ last_day_files = pg.get_last_day_files()
 # Create loop to generate arbor_evaluated_parameters array
 
 for arbor in last_day_files[0:5]:
-    alphas = np.arange(0, 1.01, 0.01)
+    alphas = np.round(np.arange(0, 1.01, 0.01), 2)
     arbor_evaluated_parameters = []
 
     for a in alphas:
@@ -24,10 +24,5 @@ for arbor in last_day_files[0:5]:
     filepath = r"C:\Users\alyan\Downloads\Research\Summer 2026\Plant-Architecture\heterogeneity_" + "\\" + arbor
     with open(filepath, "w", newline="") as file:
         writer = csv.writer(file)
+        writer.writerow(["Model", "Alpha", "Wiring Cost", "Conduction Delay", "Total Orthogonal Distance", "Total Squared Orthogonal Distance"])
         writer.writerows(arbor_evaluated_parameters)
-
-
-
-
-# second draft
-
