b9443d84eea8886f457220de4de362ede774284a
martineza088 <martineza@southwestern.edu>
2026-06-04 11:58:14 -0500

    New version of csv generation code that skips an arbor file if it does not have a reconstruction

--- heterogeneity_analysis_csv_gen.py
+++ heterogeneity_analysis_csv_gen.py
@@ -2,9 +2,11 @@ import csv
 import math
 import networkx as nx
 import numpy as np
+import pareto_functions as pf
 import plotly.graph_objs as go
 import pylab
 import plant_gravitropism as pg
+import read_arbor_reconstruction as rar
 
 
 last_day_files = pg.get_last_day_files()
@@ -33,15 +35,23 @@ last_day_files = pg.get_last_day_files()
 
 # Version 2
 alphas = np.round(np.arange(0, 1.01, 0.01), 2)
+count = 0
 
-for arbor in last_day_files:
-    arbor_evaluated_parameters = []
+for arbor in last_day_files[33:39]:
+    if rar.has_reconstruction(arbor):
+        count += 1
+        arbor_evaluated_parameters = []
 
-    filepath = r"C:\Users\alyan\Downloads\Research\Summer 2026\Plant-Architecture\heterogeneity-analysis" + "\\" + arbor
-    with open(filepath, "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Model", "Alpha", "Wiring Cost", "Conduction Delay", "Total Orthogonal Distance", "Total Squared Orthogonal Distance"])
+        filepath = r"heterogeneity-analysis" + "\\" + arbor
+        with open(filepath, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Model", "Alpha", "Wiring Cost", "Conduction Delay", "Total Orthogonal Distance", "Total Squared Orthogonal Distance"])
+            
+            for a in alphas:
+                output = pg.evaluate_parameters(arbor, 0, a)
+                writer.writerow(("optimal", a) + output)
         
-        for a in alphas:
-            output = pg.evaluate_parameters(arbor, 0, a)
-            writer.writerow(("oberved", a) + output)
+            writer.writerow(("observed", "") + (pf.wiring_cost(arbor), pf.conduction_delay(arbor)))
+        print(f"{arbor} generated (File # {count}).")
+    else:
+        print(f"Skipped {arbor}")
