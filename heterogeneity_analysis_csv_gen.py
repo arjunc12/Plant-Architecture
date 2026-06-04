bf9f801d23c1c360be0fbadfd4504cf0d332f1bd
martineza088 <martineza@southwestern.edu>
2026-06-04 13:58:31 -0500

    Used CodeX to figure out how to get the arbor networkx object. It edited line 35 in pareto_functions and changed the way main root base is retrieved. This fixed the issue with running read_arbor_full.

--- heterogeneity_analysis_csv_gen.py
+++ heterogeneity_analysis_csv_gen.py
@@ -11,33 +11,12 @@ import read_arbor_reconstruction as rar
 
 last_day_files = pg.get_last_day_files()
 
-# Create loop to generate arbor_evaluated_parameters array
+# Loop to generate arbor_evaluated_parameters array
 
-# # Version 1
-
-# for arbor in last_day_files[0:5]:
-#     alphas = np.round(np.arange(0, 1.01, 0.01), 2)
-#     arbor_evaluated_parameters = []
-
-#     for a in alphas:
-#         output = pg.evaluate_parameters(arbor, 0, a)
-
-#         arbor_values = ("observed", a) + output
-#         arbor_evaluated_parameters.append(arbor_values)
-    
-#     filepath = r"C:\Users\alyan\Downloads\Research\Summer 2026\Plant-Architecture\heterogeneity-analysis" + "\\" + arbor
-#     with open(filepath, "w", newline="") as file:
-#         writer = csv.writer(file)
-#         writer.writerow(["Model", "Alpha", "Wiring Cost", "Conduction Delay", "Total Orthogonal Distance", "Total Squared Orthogonal Distance"])
-#         writer.writerows(arbor_evaluated_parameters)
-
-
-
-# Version 2
 alphas = np.round(np.arange(0, 1.01, 0.01), 2)
 count = 0
 
-for arbor in last_day_files[33:39]:
+for arbor in last_day_files:
     if rar.has_reconstruction(arbor):
         count += 1
         arbor_evaluated_parameters = []
@@ -50,8 +29,10 @@ for arbor in last_day_files[33:39]:
             for a in alphas:
                 output = pg.evaluate_parameters(arbor, 0, a)
                 writer.writerow(("optimal", a) + output)
+            
+            arbor_object = rar.read_arbor_full(arbor)
+            writer.writerow(("observed", "") + (pf.wiring_cost(arbor_object), pf.conduction_delay(arbor_object)))
         
-            writer.writerow(("observed", "") + (pf.wiring_cost(arbor), pf.conduction_delay(arbor)))
         print(f"{arbor} generated (File # {count}).")
     else:
         print(f"Skipped {arbor}")
