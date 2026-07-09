03d85b672521adf5d53e610e687c50a31cee0e65
martineza088 <martineza@southwestern.edu>
2026-07-09 09:49:40 -0500

    script can now handle extraction for multiple files

--- optimal_alpha_csv_gen.py
+++ optimal_alpha_csv_gen.py
@@ -1,63 +1,76 @@
 import sys
 import csv
 from constants import EVALUATED_COSTS_DIR
+from pathlib import Path
 
 # get all generated files in gravitropism_pareto_fronts
-def extract_optimal_alpha_vals():
+def extract_optimal_dist_vals(fname):
     '''
-    Reads through all of the generated CSVs in the gravitropism_pareto_fronts folder, which contains evaluated costs for 
-    each arbor, and extracts them. All of these values will be organized into a new CSV file. 
+    Reads through a generated CSV in the gravitropism_pareto_fronts folder, which contains evaluated costs for 
+    each arbor, and extracts information related to the minimum squared orthogonal distance in the file for each 
+    computation method.  
 
     '''
 
     # go through all CSVs in the gravitropism_pareto_fronts folder
     #with open('%s/%s' % (EVALUATED_COSTS_DIR, fname)) as f:
-    with open('data/results/gravitropism_pareto_fronts/pimpi_ABA_D9_set1_day9_20220610_RSA_M248M058LA1511_ABA_Salt003_M248_8_C_10aba.csv') as f:
+    with open(fname) as f:
         arbor_csv = csv.reader(f)
         next(arbor_csv)
         csv_lines = []
 
-        opt_info = []
-        opt_sq_orthog_dist = float('inf')
+        homog_opt_info = []
+        homog_opt_sq_orthog_dist = float('inf')
+
+        heterog_opt_info = []
+        heterog_opt_sq_orthog_dist = float('inf')
 
         for line in arbor_csv:
 
-            #line = line.strip('\n')
-            #line = line.split(',')
             csv_lines.append(line)
 
+            cost_method = line[1]
             # checking if the current squared orthogonal distance is the smallest
-        
-            print(f"this is sq_orthog: {line[-1].strip()}")
-            print(f"    and this is opt_sq_orthog_dist: {opt_sq_orthog_dist}")
             sq_orthog = float(line[-1].strip())
 
-            # skipping the sq. orthogonal distance at the observed arbor values
-            if sq_orthog != 0 and sq_orthog < opt_sq_orthog_dist:
-                    opt_info = line[0:-1] # 
-                    opt_sq_orthog_dist = sq_orthog
-            '''
-            try:
-                print(f"this is sq_orthog: {line[-1].strip()}")
-                sq_orthog = float(line[-1].strip())
+            if cost_method == ' homogeneous':
+                 if sq_orthog != 0 and sq_orthog < homog_opt_sq_orthog_dist:  # skipping the sq. orthogonal distance at the observed arbor values
+                    homog_opt_info = [fname, line[1], line[2], line[3], line[-1]] # extracting cost method, G, alpha, and sq. orthogonal distance
+                    homog_opt_sq_orthog_dist = sq_orthog
             else:
-                if sq_orthog != 0 and sq_orthog < opt_sq_orthog_dist:
-                    opt_info = line
-                    opt_sq_orthog_dist = line[-1]
-            '''
-        alpha_vals = [row[3] for row in csv_lines]
-        #print(f"All lines: {csv_lines}\n")
-        print(f"Extracted values: {alpha_vals}")
-        print(f"Optimal squared orthogonal distance: {opt_sq_orthog_dist}")
-        print(f"Info related to optimal orthog: {opt_info}")
+                if sq_orthog != 0 and sq_orthog < heterog_opt_sq_orthog_dist:
+                    heterog_opt_info = [fname, line[1], line[2], line[3], line[-1]]
+                    heterog_opt_sq_orthog_dist = sq_orthog
+            
 
+        print(f"HOMOGENEOUS Info related to optimal orthog: {homog_opt_info}")
+        print(f"HETEROGENEOUS Info related to optimal orthog: {heterog_opt_info}")
+
+        return homog_opt_info, heterog_opt_info
+
+
+def construct_CSV(arbor_folder):
+        folder = Path(arbor_folder)
+        csv_content = [["arbor", "cost method", "G", "alpha", "total squared orthogonal distance"]]
+
+        for arbor_file in folder.glob("*.csv"):
+            print(f"This is arbor file: {arbor_file}")
+            homog_opt_info, heterog_opt_info = extract_optimal_dist_vals(arbor_file)
+            csv_content.append(homog_opt_info)
+            csv_content.append(heterog_opt_info)
         
+        print(csv_content)
+
+        with open("data/results/heterogeneous_results", "w", newline="") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(csv_content)
 
 
 def main():
-    print("Compiling all optimal heterogeneous/homogeneous alpha values...")
-    extract_optimal_alpha_vals()
-
+    print("Compiling all optimal heterogeneous/homogeneous alpha values...\n")
+    #construct_CSV(EVALUATED_COSTS_DIR)
+    construct_CSV("data/results/hetero_and_homogeneous")
+    print("\nDone.")
 
 if __name__ == '__main__':
     
