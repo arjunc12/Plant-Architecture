24d5a961b89a19f21f532f154b74d9356447df0a
martineza088 <martineza@southwestern.edu>
2026-07-13 09:53:58 -0500

    changes to create a csv that also calculates the differences between alpha values and squared orthogonal distances of both the homogeneous and heterogeneous computation methods

--- optimal_alpha_csv_gen.py
+++ optimal_alpha_csv_gen.py
@@ -42,30 +42,48 @@ def extract_optimal_dist_vals(fname, name_len):
                     heterog_opt_info = [fname_str[name_len+1:], line[1], line[2], line[3], line[-1]]
                     heterog_opt_sq_orthog_dist = sq_orthog
             
+        # tracking difference between the alpha and squared orthogonal distances of both computation methods
+        print(f"this is homog_opt_info[3]: {homog_opt_info[3]}")
+        print(f"this is heterog_opt_info[3]: {heterog_opt_info[3]}")
 
-        return homog_opt_info, heterog_opt_info
+        alpha_diff = abs(float(homog_opt_info[3]) - float(heterog_opt_info[3]))
+        sq_orthog_dist_diff = abs(homog_opt_sq_orthog_dist - heterog_opt_sq_orthog_dist)
+
+        return homog_opt_info, heterog_opt_info, alpha_diff, sq_orthog_dist_diff
 
 
 def construct_CSV(arbor_folder):
         '''
         Navigates through all the CSV files in the gravitropism_pareto_fronts folder and compiles all the 
         necessary information associated with the minimum sq. orthogonal distance into one CSV. 
+
+        This function also creates a CSV file containing the differences between the alpha values and 
+        squared orthogonal distance of each computation method. 
         '''
         folder = Path(arbor_folder)
         name_len = len(arbor_folder)
         csv_content = [["arbor", "cost method", "G", "alpha", "total squared orthogonal distance"]]
+        diff_csv = [["arbor", "alpha difference", "squared orthogonal distance difference"]]
 
         # navigate through all files in the folder
-        for arbor_file in folder.glob("*.csv"):
+        #for arbor_file in folder.glob("*.csv"):
+        for i, arbor_file in enumerate(folder.glob("*.csv")):
+            if i >= 1000:
+                break
 
-            homog_opt_info, heterog_opt_info = extract_optimal_dist_vals(arbor_file, name_len)
+            homog_opt_info, heterog_opt_info, alpha_diff, sq_orthog_dist_diff = extract_optimal_dist_vals(arbor_file, name_len)
             csv_content.append(homog_opt_info)
             csv_content.append(heterog_opt_info)
+            diff_csv.append([homog_opt_info[0], alpha_diff, sq_orthog_dist_diff])
         
         # write all relevant info into the CSV
         with open("data/results/heterogeneous_results/optimal_distance_values.csv", "w", newline="") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(csv_content)
+        
+        with open("data/results/heterogeneous_results/optimal_distance_values_diff.csv", "w", newline="") as csv_diff:
+            writer = csv.writer(csv_diff)
+            writer.writerows(diff_csv)
 
 
 def main():
